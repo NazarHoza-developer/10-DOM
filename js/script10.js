b657f175e07876b427c14eea44f4e9d584cfee0c
nazarhoza <nazarhoza@gmail.com>
2022-09-16 15:54:03 +0300

    new

---
+++ js/script10.js
@@ -0,0 +1,22 @@
+function addDiv() {
+    let div = document.createElement('div');
+    document.body.append(div);
+    div.classList.add("box");
+    let divBox = document.querySelector(".box");
+    setInterval(function(){
+        divBox.innerHTML = "";
+        for (let i = 0; i < 25; i++) {
+            divBox.innerHTML += `<div class = "child" style="background-color: ${getRandomColor()};"></div>`;
+           } 
+    }, 500);
+    divBox.innerHTML = "";
+    for (let i = 0; i < 25; i++) {
+     divBox.innerHTML += `<div class = "child" style="background-color: ${getRandomColor()};"></div>`; 
+
+    } 
+  };
+function getRandomColor() { 
+    let arrColors = ["red", "white", "blue", "orange", "green"];
+    console.log(Math.random(0, 4))
+    return arrColors[Math.floor(Math.random(0, 4) * 10)];
+}
